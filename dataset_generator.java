c3b746b5b8c5f53549cf49981032d968cfd34128
Ansonlim2311 <ansonlim2311@gmail.com>
2025-05-11 14:50:37 +0800

    update dataset code

--- dataset_generator.java
+++ dataset_generator.java
@@ -4,33 +4,27 @@ import java.util.*;
 
 public class dataset_generator {
     static final int maxValue = 1_000_000_000;
-    // static final int minStringLen = 5;
-    // static final int maxStringLen = 10;
-    // int randomLength = rand.nextInt(9) + 1;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the number of elements to generate: ");
-        int size = scanner.nextInt();
+        int n = scanner.nextInt();
         scanner.nextLine();
 
-        // System.out.print("Enter the filename to save the dataset (e.g. dataset_n.csv): ");
-        // String filename = scanner.nextLine();
-        String filename = "dataset_" + size + ".csv";
+        String filename = "dataset_" + n + ".csv";
 
-        generateDataset(filename, size);
+        generateDataset(filename, n);
 
         scanner.close();
     }
 
-    public static void generateDataset(String filename, int size) {
+    public static void generateDataset(String filename, int n) {
         Random rand = new Random();
-        Set<Integer> uniqueNumbers = new HashSet<>(size);
-        List<String> data = new ArrayList<>(size);
+        Set<Integer> uniqueNumbers = new HashSet<>(n);
+        List<String> data = new ArrayList<>(n);
 
-        System.out.println("Generating data...");
-        while (uniqueNumbers.size() < size) {
+        while (uniqueNumbers.size() < n) {
             int num = rand.nextInt(maxValue) + 1;
             if (uniqueNumbers.add(num)) {
                 String randomString = generateRandomString(rand);
@@ -38,10 +32,8 @@ public class dataset_generator {
             }
         }
 
-        System.out.println("Shuffling data...");
         Collections.shuffle(data);
 
-        System.out.println("Writing to file...");
         try (FileWriter writer = new FileWriter(filename)) {
             for (String line : data) {
                 writer.write(line + "\n");
